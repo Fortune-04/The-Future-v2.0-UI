61452ba8a8825289ea8f83d6edacb9105af660e0
Fortune-04 <wif180006@siswa.um.edu.my>
2022-09-08 15:23:48 +0800

    Update Donation.js

--- src/Components/Donation.js
+++ src/Components/Donation.js
@@ -4,55 +4,59 @@ import React,{ useState, useEffect } from "react";
 import DonationFinder from "../Apis/DonationFinder";
 
 /*Import from Material UI*/
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Divider from '@mui/material/Divider';
 import Stack from '@mui/material/Stack';
-import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 
 const Donation = () => {
 
     const [amount_donated, setAmount] = useState();
     const [temp, setTemp] = useState();
-    const [toUpdate, setToUpdate] = useState(false);
+    // const [date, setDate] = useState("");
 
     const handleSubmit = async(e) =>{
         e.preventDefault()
+
         let total = temp - amount_donated;
         try {
-            const response = await DonationFinder.put(`/update/1`, {amount_donated, total})
-            setToUpdate(true);
+            await DonationFinder.put(`/update/1`, {amount_donated, total})
             setAmount(0);
+            fetchData()
         } catch (error) {
             console.log(error)
         }
     }
 
-    useEffect(() => {
-
-        const fetchData = async () => {
+    const fetchData = async () => {
         try {
             const response = await DonationFinder.get("/")
             console.log(response.data.length);
             setTemp(response.data[0].total);
-            setToUpdate(false);
+            calculateHour(response.data[0].updated_at)
         } catch (err) {
             console.error(err.message);
         }
-        };
+    };
+
+    const calculateHour = (pastDate) => {
+        let today = new Date();
+        let past = new Date(pastDate);
+        let timeDif = (today - past)/3600000;
+
+        // if(timeDif > 24){
+
+        // }
+
+    }
+
+    useEffect(() => {
 
         fetchData();
 
-    },[toUpdate])
+    },[])
 
     return(
         <>
